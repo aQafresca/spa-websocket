41b1a45403af1163f65ba032c2bc01f2cbb132ac
Siarhei Buiko <innxfect@gmail.com>
2025-12-16 13:00:31 +0300

    JST-2586 feat(shared): add constants

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,10 @@
+export const LocalStorageKeys = {
+  TOKEN: 'auth-token',
+};
+
+export const UrlEndpoints = {
+  PRODUCTS: '/products',
+};
+
+export const DOTS = '...';
+export const LIMIT_PER_PAGE = 10;
