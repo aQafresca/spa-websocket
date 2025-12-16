37ce42396f8885d008db703a9c30049269063b60
Siarhei Buiko <innxfect@gmail.com>
2025-12-16 13:34:19 +0300

    JST-2586 feat(project): add tsconfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint2';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     tailwindcss(),
     tanstackRouter({
       target: 'react',
