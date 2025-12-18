3bde3495b3b97acd98d8f8f4a3958b153c4982e9
Siarhei Buiko <innxfect@gmail.com>
2025-12-18 20:00:01 +0300

    JST-2586 refactor(shared): update loader component

--- src/shared/components/loader/index.tsx
+++ src/shared/components/loader/index.tsx
@@ -1,7 +1,7 @@
 export const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="w-12 h-12 border-4 border-orange-400 border-t-transparent rounded-full animate-spin"></div>
+    <div>
+      <div>LOADING</div>
     </div>
   );
 };
