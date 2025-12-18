0f23613fd8baf9cb615d7d7eb11e9ba6762f32d3
Siarhei Buiko <innxfect@gmail.com>
2025-12-18 20:11:13 +0300

    JST-2586 refactor(pages): update home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,15 @@
-import { ProductListContainer } from '@/features/product-list/productListContainer.tsx';
+import { useProductListRest } from '@/features/product-list/model/useProductListRest.ts';
+import { ProductListContainer } from '@/features/product-list/ui/productListContainer.tsx';
+import { Route } from '@/routes';
 
 const HomePage = () => {
+  const { search } = Route.useSearch();
+
+  const restState = useProductListRest();
+
   return (
     <div>
-      <div>HomePage</div>
-      <ProductListContainer />
+      <ProductListContainer state={restState} search={search} />
     </div>
   );
 };
--- src/pages/home/search.shema.ts
+++ src/pages/home/search.shema.ts
@@ -2,4 +2,7 @@ import { z } from 'zod';
 
 export const searchProductsSchema = z.object({
   page: z.coerce.number().int().positive().catch(1).default(1),
+  search: z.string().catch('').default(''),
 });
+
+export const defaultSearchValues = { page: 1, search: '' };
