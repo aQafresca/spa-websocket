d7dde0eb0cd84d6e2d520be0cc7b3e75796d72b1
Siarhei Buiko <innxfect@gmail.com>
2025-12-19 17:03:24 +0300

    JST-2586 feat(project): add GlobalErrorBoundary and ToastProvider to app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App.tsx';
+import { GlobalErrorBoundary } from '@/app/providers/errorBoundaryProvider.tsx';
+import { ToastProvider } from '@/app/providers/toastProvider.tsx';
+import { queryClient } from '@/app/queryClient.ts';
 
-const queryClient = new QueryClient();
+import { App } from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <GlobalErrorBoundary>
+        <ToastProvider />
+        <App />
+      </GlobalErrorBoundary>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
