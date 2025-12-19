eef682d01b033de1d2f1579c5efc711e38b999d7
Siarhei Buiko <innxfect@gmail.com>
2025-12-19 16:53:24 +0300

    JST-2586 feat(pages): add not found page

---
+++ src/pages/not-found/index.tsx
@@ -0,0 +1,10 @@
+import { ButtonElement } from '@/shared/components/button';
+
+export const NotFoundPage = () => {
+  return (
+    <>
+      <div>NotFoundPage</div>
+      <ButtonElement>Back Home</ButtonElement>
+    </>
+  );
+};
