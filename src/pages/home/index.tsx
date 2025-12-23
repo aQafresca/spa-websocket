8722c86d241df67ce69e2a51e381019dc6ee5de4
Siarhei Buiko <innxfect@gmail.com>
2025-12-23 14:51:40 +0300

    JST-2586 refactor(pages): update home page according new update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,39 @@
+import type { IProducts } from '@/entities/products/model';
+import { ProductCard } from '@/entities/products/ui/card.tsx';
 import { useProductListRest } from '@/features/product-list/model/useProductListRest.ts';
-import { ProductListContainer } from '@/features/product-list/ui/productListContainer.tsx';
 import { Route } from '@/routes';
+import { ListContainer } from '@/shared/components/list-container/listContainer.tsx';
+import { SearchForm } from '@/shared/components/search-form';
+import { useListParams } from '@/shared/hooks/useListParams.ts';
 
 const HomePage = () => {
-  const { search } = Route.useSearch();
+  const { page, search } = Route.useSearch();
+  const navigate = Route.useNavigate();
 
-  const restState = useProductListRest();
+  const listParams = useListParams({
+    params: { page, search: search },
+    onChange: (params) => navigate({ search: () => params }),
+  });
+
+  const state = useProductListRest({ page: listParams.page, search: listParams.search });
 
   return (
     <div>
-      <ProductListContainer state={restState} search={search} />
+      <SearchForm
+        label={'products'}
+        initialValue={listParams.search}
+        placeholder={'enter product'}
+        onSubmit={listParams.handleSearch}
+      />
+      <ListContainer<IProducts>
+        state={{
+          ...state,
+          handlePageChange: listParams.handlePageChange,
+          handleSearch: listParams.handleSearch,
+        }}
+        emptyMessage={`Product ${search} not found.`}
+        renderItem={(product) => <ProductCard key={product.id} {...product} />}
+      />
     </div>
   );
 };
