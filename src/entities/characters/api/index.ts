aa91fed94105a57052f15c8d7140c06673a742ba
Siarhei Buiko <innxfect@gmail.com>
2025-12-26 18:15:35 +0300

    JST-2586 feat(entities/characters): add barrel export for api

---
+++ src/entities/characters/api/index.ts
@@ -0,0 +1,5 @@
+export * from './getCharactersByIdQueries.ts';
+export * from './getCharactersQueries.ts';
+export * from './useCharacterById.ts';
+export * from './useCharactersQuery.ts';
+export * from './characters.service.ts';
