f82ecab52bf2ee1c0f8fdf4f87741c09812e4c79
Siarhei Buiko <innxfect@gmail.com>
2025-12-26 18:03:10 +0300

    JST-2586 refactor(shared/routes): unify exports in index file

---
+++ src/shared/routes/index.ts
@@ -0,0 +1,4 @@
+export { Route as CharacterDetailRoute } from '@/routes/character/$characterId';
+export { Route as CharacterRoute } from '@/routes/graphql.tsx';
+export { Route as ProductRoute } from '@/routes';
+export { Route as ProductDetailRoute } from '@/routes/product/$productId.tsx';
