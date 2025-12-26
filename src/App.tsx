5ca9e04bcbc39f7dbc2acc699dc5fbbc8dfabab4
Siarhei Buiko <innxfect@gmail.com>
2025-12-26 18:39:45 +0300

    JST-2586 feat(project): enable global preloading and inject queryClient into router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 
+import { queryClient } from '@/app/queryClient.ts';
 import { routeTree } from '@/routeTree.gen.ts';
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  context: { queryClient: queryClient },
+  defaultPreload: 'intent',
+  defaultPreloadDelay: 300,
+});
 
 declare module '@tanstack/react-router' {
   interface Register {
