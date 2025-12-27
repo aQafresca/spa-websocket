247c0e0f0fdee954f7fe4eb3f51d95c89decf563
Siarhei Buiko <innxfect@gmail.com>
2025-12-27 15:17:14 +0300

    JST-2586 chore(shared/components): add onClose functionality

--- src/pages/detail/character/index.tsx
+++ src/pages/detail/character/index.tsx
@@ -1,14 +1,17 @@
 import { useCharacterById } from '@/entities/characters/api';
 import { CharacterDetailInfo } from '@/entities/characters/ui';
 import { DetailLayout } from '@/shared/components/detail-layout';
-import { CharacterDetailRoute } from '@/shared/routes';
+import { useHandleClose } from '@/shared/hooks';
+import { CharacterDetailRoute, CharacterRoute } from '@/shared/routes';
 
 const CharacterDetailPage = () => {
   const { characterId } = CharacterDetailRoute.useParams();
   const { data } = useCharacterById(characterId);
 
+  const handleClose = useHandleClose(CharacterRoute.to);
+
   return (
-    <DetailLayout title={data.name}>
+    <DetailLayout onClose={handleClose} title={data.name}>
       <CharacterDetailInfo {...data} />
     </DetailLayout>
   );
--- src/pages/detail/product/index.tsx
+++ src/pages/detail/product/index.tsx
@@ -1,14 +1,17 @@
 import { useProductById } from '@/entities/products/api';
 import { ProductDetailInfo } from '@/entities/products/ui';
 import { DetailLayout } from '@/shared/components/detail-layout';
-import { ProductDetailRoute } from '@/shared/routes';
+import { useHandleClose } from '@/shared/hooks';
+import { ProductRoute, ProductDetailRoute } from '@/shared/routes';
 
 const ProductDetailPage = () => {
   const { productId } = ProductDetailRoute.useParams();
   const { data } = useProductById(productId);
 
+  const handleClose = useHandleClose(ProductRoute.to);
+
   return (
-    <DetailLayout title={data.title}>
+    <DetailLayout onClose={handleClose} title={data.title}>
       <ProductDetailInfo {...data} />
     </DetailLayout>
   );
