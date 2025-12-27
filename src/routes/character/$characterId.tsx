1eacc98ddfe13bdeb0692d4c1cd3a80a81b1d457
Siarhei Buiko <innxfect@gmail.com>
2025-12-27 15:20:57 +0300

    JST-2586 refactor(routes): rework project routes

--- src/routes/character/$characterId.tsx
+++ src/routes/character/$characterId.tsx
@@ -1,13 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { productQueries } from '@/entities/products/api';
+import { characterQueries } from '@/entities/characters/api';
 import CharacterDetailPage from '@/pages/detail/character';
 import { ErrorFallback } from '@/shared/components/error-fallback';
 import { Loader } from '@/shared/components/loader';
 
 export const Route = createFileRoute('/character/$characterId')({
   loader: ({ context: { queryClient }, params: { characterId } }) =>
-    queryClient.ensureQueryData(productQueries.detail(characterId)),
+    queryClient.ensureQueryData(characterQueries.detail(characterId)),
   pendingComponent: Loader,
   component: CharacterDetailPage,
   errorComponent: ({ error, reset }) => <ErrorFallback error={error} resetErrorBoundary={reset} />,
--- src/routes/character/index.tsx
+++ src/routes/character/index.tsx
@@ -4,14 +4,10 @@ import { GraphqlPage } from '@/pages/graphql';
 import { searchCharactersSchema } from '@/pages/graphql/search.shema.ts';
 import { defaultSearchValues } from '@/pages/home/search.shema.ts';
 
-export const Route = createFileRoute('/graphql')({
-  component: RouteComponent,
+export const Route = createFileRoute('/character/')({
+  component: GraphqlPage,
   validateSearch: searchCharactersSchema,
   search: {
     middlewares: [stripSearchParams(defaultSearchValues)],
   },
 });
-
-function RouteComponent() {
-  return <GraphqlPage />;
-}
