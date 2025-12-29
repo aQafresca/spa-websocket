8307bd69f21d67394648aa8ff72d46bb8789dc1a
Siarhei Buiko <innxfect@gmail.com>
2025-12-29 14:29:54 +0300

    JST-2586 fix(routes): default values import path in character

--- src/routes/character/index.tsx
+++ src/routes/character/index.tsx
@@ -1,8 +1,7 @@
 import { createFileRoute, stripSearchParams } from '@tanstack/react-router';
 
 import { GraphqlPage } from '@/pages/graphql';
-import { searchCharactersSchema } from '@/pages/graphql/search.shema.ts';
-import { defaultSearchValues } from '@/pages/home/search.shema.ts';
+import { searchCharactersSchema, defaultSearchValues } from '@/pages/graphql/search.schema.ts';
 
 export const Route = createFileRoute('/character/')({
   component: GraphqlPage,
