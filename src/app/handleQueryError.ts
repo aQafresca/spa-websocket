f6f352594def614f028239f0330101582fe3ab32
Siarhei Buiko <innxfect@gmail.com>
2025-12-29 14:31:35 +0300

    JST-2586 refactor(app): update handleGlobalError

--- src/app/handleQueryError.ts
+++ src/app/handleQueryError.ts
@@ -4,6 +4,9 @@ import { AppError } from '@/shared/api/appErrors.ts';
 
 export const handleGlobalError = (error: unknown) => {
   if (error instanceof AppError) {
+    if (error.status === 401) {
+      return;
+    }
     toast.error(error.message);
   } else if (error instanceof Error) {
     toast.error(error.message);
