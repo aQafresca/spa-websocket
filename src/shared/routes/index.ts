536372215b272e2b6e0495a540c220a12f009a87
Siarhei Buiko <innxfect@gmail.com>
2025-12-29 14:19:47 +0300

    JST-2586 chore(shared/routes): update index exports

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -2,3 +2,5 @@ export { Route as CharacterDetailRoute } from '@/routes/character/$characterId';
 export { Route as CharacterRoute } from '@/routes/character';
 export { Route as ProductRoute } from '@/routes';
 export { Route as ProductDetailRoute } from '@/routes/product/$productId.tsx';
+export { Route as LoginRoute } from '@/routes/_guest.login.tsx';
+export { Route as ChatRoute } from '@/routes/_auth.chat.tsx';
