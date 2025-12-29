ef209dd46db3e6780d42466f3535fbb189280f6a
Siarhei Buiko <innxfect@gmail.com>
2025-12-29 14:47:19 +0300

    JST-2586 feat(pages): add login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,16 @@
+import { LoginForm } from '@/features/auth/ui';
+import { REASON } from '@/shared/constants';
+import { LoginRoute } from '@/shared/routes';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const { reason } = LoginRoute.useSearch();
+
+  return (
+    <div>
+      {reason === REASON.AUTH_REQUIRED && <div>Please sign in to access the CHAT page</div>}
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
