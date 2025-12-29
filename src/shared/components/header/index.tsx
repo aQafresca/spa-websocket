963bd22e9ff57e0940a09474af93eb224be02957
Siarhei Buiko <innxfect@gmail.com>
2025-12-29 14:48:21 +0300

    JST-2586 feat(shared/components): add logout button logic in header

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -1,12 +1,18 @@
 import { Link } from '@tanstack/react-router';
 
+import { useAuth, useLogout } from '@/entities/session/lib';
+import { ButtonElement } from '@/shared/components/button';
+
 const Header = () => {
+  const { logout } = useLogout();
+  const { isAuth } = useAuth();
+
   return (
     <header>
       <Link to={'/'}>Home</Link>
       <Link to={'/character'}>GraphQL</Link>
       <Link to={'/chat'}>Chat</Link>
-      <Link to={'/login'}>Login</Link>
+      {isAuth ? <ButtonElement onClick={() => logout()}>Logout</ButtonElement> : <Link to={'/login'}>Login</Link>}
     </header>
   );
 };
