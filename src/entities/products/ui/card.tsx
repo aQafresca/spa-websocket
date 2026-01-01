b5039b9f0ea39a0051634de50fe6bafdf248b190
Siarhei Buiko <innxfect@gmail.com>
2026-01-01 13:23:28 +0300

    JST-2586 refactor(entities/products): update card, detail info

--- src/entities/products/ui/card.tsx
+++ src/entities/products/ui/card.tsx
@@ -1,15 +1,45 @@
 import { Link } from '@tanstack/react-router';
 
 import type { IProducts } from '@/entities/products/model';
+import { StarRating } from '@/shared/components/rating';
+import { ProductCardLabel } from '@/shared/constants';
+import { calculateDiscountedPrice } from '@/shared/lib/utils';
 import { ProductDetailRoute } from '@/shared/routes';
 
-export const ProductCard = ({ id, price, title, discountPercentage, images }: IProducts) => {
+import { PriceDisplay } from './priceDisplay.tsx';
+
+export const ProductCard = ({ id, price, title, rating, discountPercentage, images }: IProducts) => {
+  const finalPrice = calculateDiscountedPrice(price, discountPercentage);
+  const hasDiscount = discountPercentage > 0;
+
   return (
-    <Link to={ProductDetailRoute.to} params={{ productId: String(id) }} preload={'intent'}>
+    <Link
+      className={
+        'relative w-[300px] h-[450px] border border-primary rounded-md transition-transform duration-200 hover:scale-105'
+      }
+      to={ProductDetailRoute.to}
+      params={{ productId: String(id) }}
+      preload={'intent'}
+    >
+      {hasDiscount && (
+        <div className={'flex flex-col leading-none absolute bg-red-500 top-4 right-0 text-center px-4 py-1'}>
+          <span>sale</span>
+          <span>{Math.round(discountPercentage)} %</span>
+        </div>
+      )}
       <img src={images[0]} alt={title} loading="lazy" width={'300px'} height={'280px'} />
-      <h3>{title}</h3>
-      <p>{price}</p>
-      <p>{discountPercentage}</p>
+      <ul className={'flex flex-col gap-3 p-3'}>
+        <li>
+          <h3 className={'text-center'}>{title}</h3>
+        </li>
+        <li className={'flex justify-around gap-2 text-lg'}>
+          <PriceDisplay price={price} hasDiscount={hasDiscount} finalPrice={finalPrice} />
+        </li>
+        <li className={'flex gap-3 mx-auto'}>
+          <span>{ProductCardLabel.rating}</span>
+          <StarRating rating={rating} />
+        </li>
+      </ul>
     </Link>
   );
 };
