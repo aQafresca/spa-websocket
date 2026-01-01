6c92b7c15d84c93fa0a20c4e58570e3ddd8c6d0a
Siarhei Buiko <innxfect@gmail.com>
2026-01-01 12:49:22 +0300

    JST-2586 style(shared/components): change css for loader

--- src/shared/components/loader/index.tsx
+++ src/shared/components/loader/index.tsx
@@ -1,7 +1,7 @@
 export const Loader = () => {
   return (
-    <div>
-      <div>LOADING</div>
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="animate-pulse text-primary font-bold">LOADING...</div>
     </div>
   );
 };
