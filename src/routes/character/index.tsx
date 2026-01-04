eb28eb8b78eb737db0a0f2def02228030af9a027
Siarhei Buiko <innxfect@gmail.com>
2026-01-04 18:46:11 +0300

    JST-2586 chore(routes): add pendingComponent

--- src/routes/character/index.tsx
+++ src/routes/character/index.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute, stripSearchParams } from '@tanstack/react-router';
 
 import { searchCharactersSchema, defaultSearchValues } from '@/pages/graphql/search.schema.ts';
+import { Loader } from '@/shared/components/loader';
 
 export const Route = createFileRoute('/character/')({
+  pendingComponent: Loader,
   validateSearch: searchCharactersSchema,
   search: {
     middlewares: [stripSearchParams(defaultSearchValues)],
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,10 @@ import { createFileRoute, stripSearchParams } from '@tanstack/react-router';
 
 import HomePage from '@/pages/home';
 import { defaultSearchValues, searchProductsSchema } from '@/pages/home/search.schema.ts';
+import { Loader } from '@/shared/components/loader';
 
 export const Route = createFileRoute('/')({
+  pendingComponent: Loader,
   component: HomePage,
   validateSearch: searchProductsSchema,
   search: {
