d96eeeed3de2661b7bf630bb34e021737a064fea
Siarhei Buiko <innxfect@gmail.com>
2026-01-04 11:52:04 +0300

    JST-2586 feat(pages): add chat page

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,5 +1,11 @@
+import { ChatWindow } from '@/entities/chat/ui';
+
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <div className={'flex justify-center items-center w-full flex-grow'}>
+      <ChatWindow />
+    </div>
+  );
 };
 
 export default ChatPage;
