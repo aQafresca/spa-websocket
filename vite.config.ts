0484ad84574cfe9957e2e2548d5258437bcb37ee
Siarhei Buiko <innxfect@gmail.com>
2026-01-04 18:45:17 +0300

    JST-2586 chore(config): add chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,18 @@ export default defineConfig({
     react(),
     eslint({ cache: false }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
